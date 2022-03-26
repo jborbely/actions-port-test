39eff56db3c875591e5c1290f833a3ee248c7203
jborbely <joe.borbely@gmail.com>
2022-03-26 18:47:04 +1300

    create test_multiple_clients

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,8 +4,18 @@ import pytest
 
 from actions import Client
 
+
 @pytest.mark.parametrize('letter', string.ascii_letters)
 def test_client(letter):
     c = Client()
     assert c.request(letter) == letter
     c.shutdown_server()
+
+
+@pytest.mark.parametrize('letter', string.ascii_letters)
+def test_multiple_clients(letter):
+    clients = [Client() for _ in range(10)]
+    for client in clients:
+        assert client.request(letter) == letter
+    for client in clients:
+        client.shutdown_server()
