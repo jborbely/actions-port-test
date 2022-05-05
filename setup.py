85205ae07a4aa275e0dcd0bf9fd9a5ad668326a9
jborbely <joe.borbely@gmail.com>
2022-05-06 09:39:26 +1200

    attempt to fix install_requires issues

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(
     packages=['actions'],
     install_requires=[
         'msl-loadlib @ git+https://github.com/MSLNZ/msl-loadlib.git',
-        'comtypes',
+        'comtypes;sys_platform=="win32"',
         'py4j',
-        'pythonnet; python_version<=3.8',
-        'pythonnet @ git+https://github.com/pythonnet/pythonnet.git@ac75e0ccc31c2780b57c01fe134652f1f1b90466; python_version>3.8'
+        'pythonnet; python_version<="3.8"',
+        'pythonnet @ git+https://github.com/pythonnet/pythonnet.git@ac75e0ccc31c2780b57c01fe134652f1f1b90466; python_version>"3.8"'
     ],
     include_package_data=True,
 )
